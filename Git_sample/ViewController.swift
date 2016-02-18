0a1dcb77539dc88581a1fef98885617539a16f13
伊藤拓登 <itoutakuto@outlook.jp>
2016-02-18 16:20:47 +0900

    3回目

--- Git_sample/ViewController.swift
+++ Git_sample/ViewController.swift
@@ -1,19 +1,20 @@
-//
-//  ViewController.swift
-//  Git_sample
-//
-//  Created by Soya Takahashi on 2015/10/07.
-//  Copyright © 2015年 TECH::CAMP. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
+        let textLabel = makeTextLable()
+        self.view.addSubview(textLabel)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func makeTextLable() -> UILabel {
         let textLabel = UILabel(frame: CGRectMake(0, 0, 300, 100))
         textLabel.layer.position = CGPoint(x: self.view.bounds.width / 2, y: self.view.bounds.height / 2)
         textLabel.backgroundColor = UIColor.redColor()
@@ -24,12 +25,8 @@ class ViewController: UIViewController {
         textLabel.textAlignment = NSTextAlignment.Center
         textLabel.layer.masksToBounds = true
         textLabel.layer.cornerRadius = 20.0
-        self.view.addSubview(textLabel)
+        return textLabel
     }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+
     
 }
